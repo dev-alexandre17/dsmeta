c94acb2cdf58ecea3b1d66c3e898056efb15aa16
dev-alexandre17 <alexandretec77@gmail.com>
2022-07-12 22:46:21 -0700

    Project created

---
+++ backend/src/test/java/com/devalexandre17/dsmeta/ProjetoDaSemanaSpringReactDevsuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devalexandre17.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDaSemanaSpringReactDevsuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
