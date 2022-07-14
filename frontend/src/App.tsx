e7d8553b562d9e63639f1a0f5fed1d7a77575b89
dev-alexandre17 <alexandretec77@gmail.com>
2022-07-13 21:29:32 -0700

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotifcationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1> Olá mundo! </h1>
+    <>
+      <h1> Olá mundo! </h1>
+      <NotifcationButton />
+      <NotifcationButton />
+    </>
   )
 }
 
